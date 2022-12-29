07df8c9a659183af25aa265aad976eb3c4e5dbc2
unknown <starberry970310@gmail.com>
2022-12-29 15:20:37 +0900

    Feat: Add getPostList query

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,49 @@
+import { graphql } from 'gatsby';
+
 import PageTemplate from '../templates/PageTemplate';
+import ContentTemplate from '../templates/ContentTemplate';
+import PostList from 'components/Post/PostList';
 
 import { page } from './index.module.scss';
 import '../styles/index.scss';
 
-const IndexPage = () => {
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
   return (
     <main className={page}>
-      <PageTemplate></PageTemplate>
+      <PageTemplate>
+        <ContentTemplate>
+          <PostList posts={edges} />
+        </ContentTemplate>
+      </PageTemplate>
     </main>
   );
 };
 
 export default IndexPage;
+
+export const getPostList = graphql`
+  query getPostList {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData(width: 768, height: 768)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
