2c02dbb38d89510044098ec1fe56f29aaf783e46
unknown <starberry970310@gmail.com>
2022-12-29 15:25:18 +0900

    Feat: Add Post list

---
+++ src/components/Post/PostList/index.jsx
@@ -0,0 +1,21 @@
+import PostItem from './PostItem';
+
+import * as styles from './postList.module.scss';
+
+const PostList = ({ posts }) => {
+  return (
+    <section className={styles.postListWrapper}>
+      <div className={styles.header}>
+        <h2>최근 포스팅</h2>
+        <div className={styles.dropdown}>최신순</div>
+      </div>
+      <div className={styles.postList}>
+        {posts.slice(0, 10).map((post) => (
+          <PostItem post={post} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default PostList;
