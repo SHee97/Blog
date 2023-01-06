483cc8189a40190c1f1642ded4a4b228fd5500d8
unknown <starberry970310@gmail.com>
2023-01-06 15:35:19 +0900

    Feat: Add slug query

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -30,6 +30,9 @@ export const getPostList = graphql`
       edges {
         node {
           id
+          fields {
+            slug
+          }
           frontmatter {
             title
             summary
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -34,6 +34,9 @@ export const getPostList = graphql`
       edges {
         node {
           id
+          fields {
+            slug
+          }
           frontmatter {
             title
             summary
