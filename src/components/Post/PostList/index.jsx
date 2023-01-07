ed656bdd24d7e94948a20395232a8fa866f194cb
unknown <starberry970310@gmail.com>
2023-01-07 16:47:30 +0900

    Refactor: Lower queries to component level

--- src/components/Post/PostList/index.jsx
+++ src/components/Post/PostList/index.jsx
@@ -1,8 +1,37 @@
+import { useStaticQuery, graphql } from 'gatsby';
+
 import PostItem from './PostItem';
 
 import * as styles from './postList.module.scss';
 
-const PostList = ({ posts, count }) => {
+const PostList = ({ count }) => {
+  const {
+    allMarkdownRemark: { edges },
+  } = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              summary
+              date(formatString: "YYYY.MM.DD")
+              categories
+              thumbnail {
+                childImageSharp {
+                  gatsbyImageData(width: 768, height: 768)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <section className={styles.postListWrapper}>
       <div className={styles.header}>
@@ -10,7 +39,7 @@ const PostList = ({ posts, count }) => {
         <div className={styles.dropdown}>최신순</div>
       </div>
       <div className={styles.postList}>
-        {posts.slice(0, count).map((post) => (
+        {edges.slice(0, count).map((post) => (
           <PostItem post={post} />
         ))}
       </div>
