a21fbc3e9ee47e43a502b8348f91d00c75a1a219
Ok-Cheese <starberry970310@gmail.com>
2023-01-18 17:17:45 +0900

    Design: Add header title

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,6 +7,7 @@ import * as styles from './header.module.scss';
 const Header = () => {
   return (
     <header className={styles.header}>
+      <h1 className={styles.title}>SHee97's Blog</h1>
       <Link className={styles.iconWrapper} to='/'>
         <FontAwesomeIcon icon={faHome} />
       </Link>
